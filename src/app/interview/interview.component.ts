8593e6a277d666e2dc6af69c2a1f6a1c344d7ae3
tangch5861 <cheehongmalaysia@gmail.com>
2018-02-05 19:42:57 +0800

    add interview

--- src/app/interview/interview.component.ts
+++ src/app/interview/interview.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterviewComponent implements OnInit {
 
+  completed_1: boolean = true;
+  completed_2: boolean = false;
+  completed_3: boolean = false;
+  completed_4: boolean = false;
+  done_1: boolean = false;
+  done_2: boolean = false;
+  done_3: boolean = false;
+  done_4: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  GoToNext(value) {
+
+  	if(value == 1) {
+  		this.completed_1 = true;
+  		this.completed_2 = false;
+  		this.completed_3 = false;
+  		this.completed_4 = false;
+  	}
+  	if(value == 2) {
+  		this.completed_1 = false;
+  		this.completed_2 = true;
+  		this.done_1 = true;
+  		this.completed_3 = false;
+  		this.completed_4 = false;
+  	}
+  	if(value == 3) {
+  		this.completed_2 = false;
+  		this.completed_3 = true;
+  		this.done_2 = true;
+  		this.completed_4 = false;
+  		this.completed_1 = false;
+  	}
+  	if(value == 4) {
+  		this.completed_3 = false;
+  		this.completed_4 = true;
+  		this.done_3 = true;
+  		this.completed_2 = false;
+  		this.completed_1 = false;
+  	}
+	  	
+  }
+
+  InterviewCompleted(){
+
   }
 
 }
