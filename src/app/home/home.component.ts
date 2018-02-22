7a1af1255d6b36f0688debad9842c710490ee00b
tangch5861 <cheehongmalaysia@gmail.com>
2018-02-22 21:58:28 +0800

    change icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	  this.load_js();
   }
+
+	load_js()
+   	{
+  		//use to reload the bundle external js scripts
+      var head= document.getElementsByTagName('head')[0];
+      var script= document.createElement('script');
+      script.type= 'text/javascript';
+      script.src= 'scripts.bundle.js';
+      head.appendChild(script);
+   	}
 
 }
